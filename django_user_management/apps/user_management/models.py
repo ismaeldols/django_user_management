72fc01979903536c195cba5aa2d18a89bf948e09
Ismael Dols Manrique <ismaeldolsmanrique@gmail.com>
2019-10-20 01:13:15 +0200

    Added IBAN required field to CustomUser

--- django_user_management/apps/user_management/models.py
+++ django_user_management/apps/user_management/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django_iban.fields import IBANField
 
 
 class CustomUser(AbstractUser):
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'email']
+    iban = IBANField(enforce_database_constraint=True, unique=True)
+    REQUIRED_FIELDS = ['iban', 'first_name', 'last_name', 'email']
